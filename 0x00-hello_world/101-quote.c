3c8b33ad15eeec85b0c9fb84d367d26cfc4022a1
arbythecoder <houseofarby@gmail.com>
2023-03-20 05:14:34 -0700

    first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,7 @@
  * Return: 1
  **/
 int main(void)
-{
- 	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, 59);
-    return 1;
+{	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ 	write(STDERR_FILENO, message, 59);
+ 	return (1);
 }
