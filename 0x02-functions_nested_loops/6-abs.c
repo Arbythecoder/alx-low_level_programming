bcfc5db36d6f402d838f6a1cf2fe78689f972ccb
arbythecoder <houseofarby@gmail.com>
2023-03-22 18:39:10 -0700

    task 6 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - prints te absolute value
+ * @n: input parameter
+ * codes written by arbythecoder
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
