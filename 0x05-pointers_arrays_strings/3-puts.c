0f1804e9769e52da4df2f2675fa416859a5b6b03
arbythecoder <houseofarby@gmail.com>
2023-03-28 14:25:09 -0700

    3-puts.c file added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * @arbythecoder
+ * Return: string and new line
+ */
+void _puts(char *str)
+{
+	int i =0;
+	
+	while (str[i])
+	{ 	
+		_putchar(str[i]);
+		i++;
+
+	}
+	_putchar ('\n');
+}
