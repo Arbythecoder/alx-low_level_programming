39e3b53db07cdf8645c159ec820e9a47d3602d8e
arbythecoder <houseofarby@gmail.com>
2023-04-04 17:22:34 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,23 +2,22 @@
 
 /**
 * _strchr -Locate a character in as string.
-* @s: The string to be searched.
+* @s: pointer to char
 * @c: The character to be located.
 *
 * Return: if c is found - a pointer to the first occurence.
 * if c is not found - NULL.
 */
 
-char *_strchr(char *s,char c)
+char *_strchr(char *s, char c)
 {
-      int index;
-
-      for (index = 0; s[index] >= '\0'; index++)
-      {
-             if (s[index] == c)
-                    return (s + index);
-       }
-    
-       return('/0');
+    while (*s != '\0')
+    {
+        if (*s == c)
+        {
+            return s;
+        }
+        s++;
+    }
+    return NULL;
 }
-
