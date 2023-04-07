7f5fb37d9f8924e46c389e142eae8a2dbe6446d1
arbythecoder <houseofarby@gmail.com>
2023-04-07 15:30:40 -0700

    _print_rev_recursion added successfully

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,17 +2,15 @@
 
 /**
 * _print_rev_recursion.c -prints a string in reverse.
-* @s: The string to be printed.
-* Return:void
+* @s:  string to print
 */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s !='\0');
-
-	return;
+	if (*s)
+	{
 	_print_rev_recursion(s + 1);
-	else
 	_putchar(*s);
+	}
 }
 
