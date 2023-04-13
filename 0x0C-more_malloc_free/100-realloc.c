885989cb167c6d02c24cbfbc871eefab87403f36
arbythecoder <houseofarby@gmail.com>
2023-04-13 15:44:02 -0700

    0x0C-more malloc free file added

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,3 +24,22 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
             return NULL;
         return new_ptr;
     }
+	if (new_size == 0)
+    {
+        free(ptr);
+        return NULL;
+    }
+
+    new_ptr = malloc(new_size);
+    if (new_ptr == NULL)
+        return NULL;
+
+    if (new_size > old_size)
+        memcpy(new_ptr, ptr, old_size);
+    else
+        memcpy(new_ptr, ptr, new_size);
+
+	free(ptr);
+
+	 return new_ptr;
+}
