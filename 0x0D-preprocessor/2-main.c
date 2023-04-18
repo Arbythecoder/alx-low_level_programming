4d2f3f15bfc6218244f0a707aabe43d2a6f65dcf
arbythecoder <houseofarby@gmail.com>
2023-04-18 13:25:59 -0700

    0x0D-preprocessor file added

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the name of the file it was compiled from
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("File : %s\n", FILE);
+	return 0;
+}
